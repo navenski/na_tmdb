8f6c77502833f25e6f85d47598bf51cc3dd8ce92
sl-navekportella <navek.portella@groupeseloger.com>
2021-09-19 01:41:58 +0200

    wip

---
+++ data/src/main/java/com/navektest/data/inject/RepositoryModule.kt
@@ -0,0 +1,11 @@
+package com.navektest.data.inject
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object RepositoryModule {
+
+}
