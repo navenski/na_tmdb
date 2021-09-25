2fe760208586a7d84b41ffe801ea7196f19416df
sl-navekportella <navek.portella@groupeseloger.com>
2021-09-25 17:44:55 +0200

    refactor DiscoverMovieUseCase.kt

--- domain/usecase/src/main/java/com/navektest/usecase/common/UseCase.kt
+++ domain/usecase/src/main/java/com/navektest/usecase/common/UseCase.kt
@@ -3,5 +3,10 @@ package com.navektest.usecase.common
 import kotlinx.coroutines.flow.Flow
 
 interface UseCase<in P, out T> {
-    fun execute(parameter: P) : Flow<T>
+    fun execute(parameter: P): Flow<T>
+}
+
+interface ObservableUseCase<in P, out T> {
+    fun execute(parameter: P)
+    fun observe(): Flow<T>
 }
--- domain/usecase/src/main/java/com/navektest/usecase/discover/DiscoverMovieUseCase.kt
+++ domain/usecase/src/main/java/com/navektest/usecase/discover/DiscoverMovieUseCase.kt
@@ -3,7 +3,7 @@ package com.navektest.usecase.discover
 import com.navektest.business.discover.DiscoverRepository
 import com.navektest.business.model.MediaPage
 import com.navektest.toolkit.dispatcher.CoroutineDispatcherProvider
-import com.navektest.usecase.common.UseCase
+import com.navektest.usecase.common.ObservableUseCase
 import com.navektest.usecase.common.transformer.MediaPageConcatZipper
 import com.navektest.usecase.discover.param.DiscoverParam
 import kotlinx.coroutines.FlowPreview
@@ -20,7 +20,7 @@ import javax.inject.Inject
  * Hot Flow.
  * collect must be called once
  */
-interface DiscoverMovieUseCase : UseCase<DiscoverParam, MediaPage>
+interface DiscoverMovieUseCase : ObservableUseCase<DiscoverParam, MediaPage>
 
 @OptIn(FlowPreview::class)
 internal class DiscoverMovieUseCaseImpl @Inject constructor(private val repository: DiscoverRepository,
@@ -29,15 +29,13 @@ internal class DiscoverMovieUseCaseImpl @Inject constructor(private val reposito
 ) :
     DiscoverMovieUseCase {
 
-    private val sharedFlow = MutableSharedFlow<DiscoverParam>()
+    private val sharedFlow = MutableSharedFlow<DiscoverParam>(replay = 1)
 
     //Init with an empty page
     @Volatile private var lastMediaPage: MediaPage = MediaPage(1, emptyList(), 1, 0)
 
-    override fun execute(parameter: DiscoverParam): Flow<MediaPage> {
-        sharedFlow.tryEmit(parameter)
-
-        return sharedFlow.flatMapMerge {
+    private val mediaPageFlow by lazy {
+        sharedFlow.flatMapMerge { parameter ->
             repository.getMovie(page = parameter.page)
         }
             .zip(getLastMediaPage(), zipper::zip)
@@ -48,4 +46,10 @@ internal class DiscoverMovieUseCaseImpl @Inject constructor(private val reposito
     }
 
     private fun getLastMediaPage(): Flow<MediaPage> = flow { emit(lastMediaPage) }
+
+    override fun execute(parameter: DiscoverParam) {
+        sharedFlow.tryEmit(parameter)
+    }
+
+    override fun observe(): Flow<MediaPage> = mediaPageFlow
 }
